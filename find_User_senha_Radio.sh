0342af083c3feeadf76cbd0004ca07973cadf386
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2018-01-04 17:10:13 -0300

    Otimizado.

--- find_User_senha_Radio.sh
+++ find_User_senha_Radio.sh
@@ -18,15 +18,16 @@ for i in $(cat ${DIR}/radios.txt); do
 	pass_find=0
 	user_find=0
 	if [ "$pass_find" -eq 0 ] && [ "$user_find" -eq  0 ]; then
-		for senha in $(cat ${DIR}/pass.txt); do
-			for usuario in $(cat ${DIR}/users.txt); do
+		for usuario in $(cat ${DIR}/users.txt); do
+			for senha in $(cat ${DIR}/pass.txt); do
 				if (sshpass -p $senha ssh $i -l $usuario -o StrictHostKeyChecking=no -oCheckHostIP=no \ "exit");	then
 					pass_find=1;
 					user_find=1;
 					echo -ne "$i,$usuario,$senha\r\n" >> senha.txt
-					break #finish loop user
+					break 2; #finish loop user
 				else
 					echo -ne "Not Found $i \r\n" >> /var/log/find_pass.$hoje.log
+					sleep 2;
 				fi
 			done #end users
 		done #end pass

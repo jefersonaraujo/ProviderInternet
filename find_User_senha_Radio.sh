576de62bf2d07791177d7dce53f7ddc45a6584b3
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2018-01-04 14:22:14 -0300

    Encontrarndo a senha e jogando em arquivo.

--- find_User_senha_Radio.sh
+++ find_User_senha_Radio.sh
@@ -14,20 +14,21 @@ pass_find=0
 user_find=0
 DIR="/tmp"
 #=======================================================================#
-for i in $(cat radios.txt); do
+for i in $(cat ${DIR}/radios.txt); do
 	pass_find=0
-	for senha in $(cat ${DIR}/pass.txt); do
-		case "$pass_find" in
-			"0")if(sshpass -p $senha ssh $i -l admin -o StrictHostKeyChecking=no -oCheckHostIP=no \
-			"exit")
-			then
-				pass_find=1;
-				echo -ne "Tarefa concluida ip $i senha $senha.\r\n" >> /var/log/log_sucesso_snmp.$hoje.log
-			else
-				echo -ne "Tarefa não concluida  ip $i $senha \r\n" >> /var/log/log_erro_snmp.$hoje.log
-				pass_find=0;
-			fi
-			;;
-		esac
-	done
+	user_find=0
+	if [ "$pass_find" -eq 0 ] && [ "$user_find" -eq  0 ]; then
+		for senha in $(cat ${DIR}/pass.txt); do
+			for usuario in $(cat ${DIR}/users.txt); do
+				if (sshpass -p $senha ssh $i -l $usuario -o StrictHostKeyChecking=no -oCheckHostIP=no \ "exit");	then
+					pass_find=1;
+					user_find=1;
+					echo -ne "$i,$usuario,$senha\r\n" >> senha.txt
+					break #finish loop user
+				else
+					echo -ne "Not Found $i \r\n" >> /var/log/find_pass.$hoje.log
+				fi
+			done #end users
+		done #end pass
+	fi
 done

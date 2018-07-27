7b4bec62ca51c8dab7ebbdbba2d9f9bcf0e7a96e
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-07-27 15:41:02 -0300

    update

--- backup-mikrotik.sh
+++ backup-mikrotik.sh
@@ -1,1 +1,15 @@
-i
+#!/bin/bash
+
+
+PASS=`minhasena`
+PORT=22
+DATE=`date +%Y%m%d`
+
+#local onde vai salvar
+mkdir -p /home/mikrotik/$DATE
+
+#precisa instalar o sshpass. 
+for IP in `cat /tmp/rb-list`; do
+
+        sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no -p $PORT -l backup $IP '/export' > /home/mikrotik/$DATE/bkp-$IP.rsc
+done

bdf0832ce81b2017a75982207e272b9c5b009e43
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-08-23 16:15:25 -0300

    update

--- trocar-snmp.sh
+++ trocar-snmp.sh
@@ -5,11 +5,9 @@ PASS=`cat /root/pf`
 PORT=22
 DATE=`date +%Y%m%d`
 
-mkdir /home/mikrotik/$DATE
-
 for IP in `cat rb-list`; do
 
-        sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no -p $PORT -l backup $IP 'snmp community set fornet addresses=192.168.X.X'   
+        sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no -p $PORT -l backup $IP 'snmp community set public  addresses=192.168.X.X'   
 
 done
 
